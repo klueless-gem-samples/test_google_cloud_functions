63a717ab123207ac07c24b0d051ff0f38f9b6a86
David Cruwys <david@ideasmen.com.au>
2022-04-17 09:42:17 +1000

    chore: opinionated gem files

--- lib/test_google_cloud_functions.rb
+++ lib/test_google_cloud_functions.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
-require_relative "test_google_cloud_functions/version"
+require_relative 'test_google_cloud_functions/version'
 
 module TestGoogleCloudFunctions
-  class Error < StandardError; end
+  # raise TestGoogleCloudFunctions::Error, 'Sample message'
+  Error = Class.new(StandardError)
+
   # Your code goes here...
+end
+
+if ENV['KLUE_DEBUG']&.to_s&.downcase == 'true'
+  namespace = 'TestGoogleCloudFunctions::Version'
+  file_path = $LOADED_FEATURES.find { |f| f.include?('test_google_cloud_functions/version') }
+  version   = TestGoogleCloudFunctions::VERSION.ljust(9)
+  puts "#{namespace.ljust(35)} : #{version.ljust(9)} : #{file_path}"
 end
